29406697509d249b3e2bc14a54f8cc7236695969
Dinara Safina <dinakhai27@gmail.com>
2024-07-13 16:28:30 +0300

    test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,13 @@
 import pytest
 from selene import browser
-from selenium import webdriver
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
-    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
-    # browser.config.timeout = 2.0
-    driver_options = webdriver.ChromeOptions()
-    driver_options.add_argument('--headless')  # вместо этой строки можно добавить другие опции
-    browser.config.driver_options = driver_options
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
 
     yield
 
     browser.quit()
-
-
--- tests/test_filling_form.py
+++ tests/test_filling_form.py
@@ -1,17 +1,30 @@
 from selene import browser, be, have
-
-
-def test_page():
-    browser.open('/')
-    browser.element('[class="text-center"]').should(have.text('Practice Form'))
+import os
 
 
 def test_filling_form():
+    browser.open('/automation-practice-form')
     browser.element('#firstName').should(be.blank).type('Dinara')
     browser.element('#lastName').should(be.blank).type('Safina')
-    browser.element('#email').should(be.blank).type('dinatest@bk.ru')
-    browser.element('#gender-radio-2').should(be.enabled)
-    browser.element('#userNumber').should(be.blank).type('+79999999999')
+    browser.element('#userEmail').should(be.blank).type('dinatest@bk.ru')
+    browser.element('[for="gender-radio-2"]').click()
+    browser.element('#userNumber').should(be.blank).type('9999999999')
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__year-select').element('[value="1999"]').click()
+    browser.element('.react-datepicker__month-select').click()
+    browser.element('.react-datepicker__month-select').element('[value="3"]').click()
+    browser.element('.react-datepicker__day--011').click()
+    browser.element('#subjectsInput').should(be.blank).type('Maths').press_enter()
+    browser.element('[for="hobbies-checkbox-1"]').click()
+    browser.element('[for="hobbies-checkbox-2"]').click()
+    browser.element('#uploadPicture').send_keys(os.path.abspath('test_image.jpg'))
+    browser.element('#currentAddress').type('Russia, Ufa, Lenina St., 1')
+    browser.element('#react-select-3-input').type('Haryana').press_enter()
+    browser.element('#react-select-4-input').type('Panipat').press_enter()
+    browser.element('#submit').click()
 
-
-
+    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
+    browser.element('.table').all('td').even.should(have.exact_texts(
+        'Dinara Safina', 'dinatest@bk.ru', 'Female', '9999999999', '11 April,1999', 'Maths', 'Sports, Reading',
+        'test_image.jpg', 'Russia, Ufa, Lenina St., 1', 'Haryana Panipat'
+    ))
