e689b3524b498d32468810cddda1e2d08415fd62
Dinara Safina <dinakhai27@gmail.com>
2024-09-03 00:22:56 +0300

    fixed mid-level

--- tests/test_filling_form.py
+++ tests/test_filling_form.py
@@ -1,30 +1,24 @@
-from selene import browser, be, have
-import os
+from modules.pages.registration_form_page import RegistrationPage
 
 
 def test_filling_form():
-    browser.open('/automation-practice-form')
-    browser.element('#firstName').should(be.blank).type('Dinara')
-    browser.element('#lastName').should(be.blank).type('Safina')
-    browser.element('#userEmail').should(be.blank).type('dinatest@bk.ru')
-    browser.element('[for="gender-radio-2"]').click()
-    browser.element('#userNumber').should(be.blank).type('9999999999')
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__year-select').element('[value="1999"]').click()
-    browser.element('.react-datepicker__month-select').click()
-    browser.element('.react-datepicker__month-select').element('[value="3"]').click()
-    browser.element('.react-datepicker__day--011').click()
-    browser.element('#subjectsInput').should(be.blank).type('Maths').press_enter()
-    browser.element('[for="hobbies-checkbox-1"]').click()
-    browser.element('[for="hobbies-checkbox-2"]').click()
-    browser.element('#uploadPicture').send_keys(os.path.abspath('test_image.jpg'))
-    browser.element('#currentAddress').type('Russia, Ufa, Lenina St., 1')
-    browser.element('#react-select-3-input').type('Haryana').press_enter()
-    browser.element('#react-select-4-input').type('Panipat').press_enter()
-    browser.element('#submit').click()
+    registration_page = RegistrationPage()
 
-    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
-    browser.element('.table').all('td').even.should(have.exact_texts(
-        'Dinara Safina', 'dinatest@bk.ru', 'Female', '9999999999', '11 April,1999', 'Maths', 'Sports, Reading',
-        'test_image.jpg', 'Russia, Ufa, Lenina St., 1', 'Haryana Panipat'
-    ))
+    registration_page.open()
+    registration_page.fill_first_name('Dinara')
+    registration_page.fill_last_name('Safina')
+    registration_page.fill_email('dinatest@bk.ru')
+    registration_page.choose_gender()
+    registration_page.fill_phone_number('9999999999')
+    registration_page.fill_date_of_birth('1999', 'April', '11')
+    registration_page.fill_subjects('Maths')
+    registration_page.fill_hobbies()
+    registration_page.upload_picture('test_image.jpg')
+    registration_page.fill_address('Russia, Ufa, Lenina St., 1')
+    registration_page.select_state('Haryana')
+    registration_page.select_city('Panipat')
+    registration_page.submit()
+
+    registration_page.assert_user_submitted_form('Dinara Safina', 'dinatest@bk.ru', 'Female', '9999999999', '11 April,1999', 'Maths', 'Sports, Reading',
+        'test_image.jpg', 'Russia, Ufa, Lenina St., 1', 'Haryana Panipat')
+
